d25fcaf8c4a701040dfe99f38818df5b15f7ea2b
Gonza390 <olmosgonza69@gmail.com>
2026-08-27 18:30:54 -0300

    Apply clean materials shell

---
+++ app/dashboard/materiales/layout.tsx
@@ -0,0 +1,5 @@
+import './materials-clean.css';
+
+export default function DashboardMaterialsLayout({ children }: { children: React.ReactNode }) {
+  return <div className="materials-editorial">{children}</div>;
+}
