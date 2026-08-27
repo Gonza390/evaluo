6a0dd125e1f2740531a7be69104907033bcf57fa
Gonza390 <olmosgonza69@gmail.com>
2026-08-27 18:55:56 -0300

    Apply clean career information shell

--- app/materias/layout.tsx
+++ app/materias/layout.tsx
@@ -1,5 +1,10 @@
 import ClientLayout from '@/components/ClientLayout';
+import './career-clean.css';
 
 export default function MateriasLayout({ children }: { children: React.ReactNode }) {
-  return <ClientLayout>{children}</ClientLayout>;
+  return (
+    <ClientLayout>
+      <div className="career-editorial">{children}</div>
+    </ClientLayout>
+  );
 }
