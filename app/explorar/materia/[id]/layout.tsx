8da5e439df4e0bf629053010b9f7773904a7ac24
Gonza390 <olmosgonza69@gmail.com>
2026-08-27 18:51:26 -0300

    Apply clean materia page shell

---
+++ app/explorar/materia/[id]/layout.tsx
@@ -0,0 +1,5 @@
+import './materia-clean.css';
+
+export default function MateriaLayout({ children }: { children: React.ReactNode }) {
+  return <div className="materia-editorial">{children}</div>;
+}
