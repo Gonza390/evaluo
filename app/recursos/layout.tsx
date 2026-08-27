4af7fbb2579dc2b1ae1ee3e817c203118464807f
Gonza390 <olmosgonza69@gmail.com>
2026-08-27 19:29:30 -0300

    Add clean reader layout

---
+++ app/recursos/layout.tsx
@@ -0,0 +1,5 @@
+import './reader-clean.css';
+
+export default function RecursosLayout({ children }: { children: React.ReactNode }) {
+  return <div className="resource-reader-clean">{children}</div>;
+}
