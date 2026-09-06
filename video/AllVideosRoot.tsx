5ed9a77d63f2b57bb783147b664d86d4ae9b31f4
Gonza390 <olmosgonza69@gmail.com>
2026-09-06 09:46:08 -0300

    Add combined Remotion root for all videos

---
+++ video/AllVideosRoot.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {RemotionRoot} from './Root';
+import {ProductDemoRoot} from './ProductDemoRoot';
+
+export const AllVideosRoot: React.FC = () => (
+  <>
+    <RemotionRoot />
+    <ProductDemoRoot />
+  </>
+);
