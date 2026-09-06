a1703d3b819b7974970caf3643f92b38ecd5c196
Gonza390 <olmosgonza69@gmail.com>
2026-09-06 07:33:56 -0300

    Use corrected Evaluo PDF video

--- video/Root.tsx
+++ video/Root.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Composition} from 'remotion';
-import {EvaluoLaunchVideo} from './EvaluoLaunchVideo';
+import {EvaluoPdfVideo} from './EvaluoPdfVideo';
 
 export const RemotionRoot: React.FC = () => {
   return (
     <Composition
       id="EvaluoLaunchVideo"
-      component={EvaluoLaunchVideo}
+      component={EvaluoPdfVideo}
       durationInFrames={900}
       fps={30}
       width={1080}
