129f4d3949b219845d2f34ec0429f7fdf1d8a4cc
Gonza390 <olmosgonza69@gmail.com>
2026-09-06 08:58:51 -0300

    Add ComfyUI Remotion demo composition

--- video/Root.tsx
+++ video/Root.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import {Composition} from 'remotion';
 import {EvaluoPdfVideoPolished} from './EvaluoPdfVideoPolished';
+import {EvaluoComfyDemo} from './EvaluoComfyDemo';
 
 export const RemotionRoot: React.FC = () => {
   return (
-    <Composition
-      id="EvaluoLaunchVideo"
-      component={EvaluoPdfVideoPolished}
-      durationInFrames={900}
-      fps={30}
-      width={1080}
-      height={1920}
-    />
+    <>
+      <Composition
+        id="EvaluoComfyDemo"
+        component={EvaluoComfyDemo}
+        durationInFrames={540}
+        fps={30}
+        width={1080}
+        height={1920}
+      />
+      <Composition
+        id="EvaluoLaunchVideo"
+        component={EvaluoPdfVideoPolished}
+        durationInFrames={900}
+        fps={30}
+        width={1080}
+        height={1920}
+      />
+    </>
   );
 };
