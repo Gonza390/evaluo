67a5eba3d6ba9589ad0d9983107ebefd58b8cde8
Gonza390 <olmosgonza69@gmail.com>
2026-09-06 09:56:05 -0300

    video: include chaos-to-study in all videos

--- video/AllVideosRoot.tsx
+++ video/AllVideosRoot.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {RemotionRoot} from './Root';
 import {ProductDemoRoot} from './ProductDemoRoot';
+import {ChaosToStudyRoot} from './ChaosToStudyRoot';
 
 export const AllVideosRoot: React.FC = () => (
   <>
     <RemotionRoot />
     <ProductDemoRoot />
+    <ChaosToStudyRoot />
   </>
 );
