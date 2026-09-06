a1f66c3a8d076472e201837b577b2275895a06e2
Gonza390 <olmosgonza69@gmail.com>
2026-09-06 07:45:39 -0300

    Use polished mouse-driven PDF video

--- video/Root.tsx
+++ video/Root.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Composition} from 'remotion';
-import {EvaluoPdfVideo} from './EvaluoPdfVideo';
+import {EvaluoPdfVideoPolished} from './EvaluoPdfVideoPolished';
 
 export const RemotionRoot: React.FC = () => {
   return (
     <Composition
       id="EvaluoLaunchVideo"
-      component={EvaluoPdfVideo}
+      component={EvaluoPdfVideoPolished}
       durationInFrames={900}
       fps={30}
       width={1080}
