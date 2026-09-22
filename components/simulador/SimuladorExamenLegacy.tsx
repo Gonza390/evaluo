34bfa3d76a6755d0910f4ef17f25134b500cc1ec
Gonza390 <olmosgonza69@gmail.com>
2026-09-22 07:55:10 -0300

    Track simulator signup CTA clicks

--- components/simulador/SimuladorExamenLegacy.tsx
+++ components/simulador/SimuladorExamenLegacy.tsx
@@ -1849,7 +1849,17 @@ export default function SimuladorExamen({
         loginHref={loginHref}
         signupHref={signupHref}
         onLoginClick={() => void emitLoginGateEvent('simulator_login_gate_cta_clicked', 'login')}
-        onSignupClick={() => void emitLoginGateEvent('simulator_login_gate_cta_clicked', 'signup')}
+        onSignupClick={() => {
+          void emitLoginGateEvent('simulator_login_gate_cta_clicked', 'signup');
+          trackMarketingEvent('signup_cta_clicked', {
+            location: 'simulator_gate',
+            source_path: pathname,
+            next_path: signupHref,
+            materia_id: materiaId,
+            parcial,
+            mode,
+          });
+        }}
         onNeedsFeedback={(reason) => emitNeedsFeedback(reason, 'gate')}
       />
     );
